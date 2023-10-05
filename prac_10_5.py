5fa9caa44070aea95465e84dbd8a75ecf118d19e
cjrcjrdl1 <88540899+cjrcjrdl1@users.noreply.github.com>
2023-10-06 00:02:40 +0900

    Update prac_10_5.py

--- prac_10_5.py
+++ prac_10_5.py
@@ -59,16 +59,16 @@ from matplotlib.image import imread
 # plt.show()
 
 x = np.arange(0,6,0.1)
-y1 = np.sin(x)
-y2 = np.cos(x)
+# y1 = np.sin(x)
+# y2 = np.cos(x)
 
-plt.plot(x,y1, label="sin")
-plt.plot(x,y2, linestyle="--", label="cos")
-plt.xlabel("x") # x축 이름
-plt.ylabel("y") # y축 이름
-plt.title('sin & cos') # 제목
-plt.legend() #범례 표시
-plt.show()
+# plt.plot(x,y1, label="sin")
+# plt.plot(x,y2, linestyle="--", label="cos")
+# plt.xlabel("x") # x축 이름
+# plt.ylabel("y") # y축 이름
+# plt.title('sin & cos') # 제목
+# plt.legend() #범례 표시
+# plt.show()
 
 img = imread('density.jpg')
 
